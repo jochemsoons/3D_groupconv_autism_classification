5474826245675525c04828c17d2601de72973000
jochemsoons <33282515+jochemsoons@users.noreply.github.com>
2021-11-19 11:33:46 +0100

    Delete reading_hd5f.py

--- reading_hd5f.py
+++
@@ -1,8 +0,0 @@
-import h5py
-import numpy as np
-
-f = h5py.File("/home/lisasalomons/Desktop/fmri_summary_abideI_II.hdf5", 'r')
-f1 = f['summaries']
-labels = f1.attrs['DX_GROUP']
-for group, i in f1:
-    
